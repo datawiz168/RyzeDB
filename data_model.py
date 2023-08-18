c3159ee7498773aee6a9381cb7c19ce2efe25984
datawiz168 <140295050+datawiz168@users.noreply.github.com>
2023-08-18 22:08:20 +0800

    Add files via upload

--- data_model.py
+++ data_model.py
@@ -1,31 +1,68 @@
-import json  # 导入JSON库，用于键值对的序列化和反序列化
+import json
+from typing import Any
 
-# KeyValue类代表了一个键值对。这是LSM-tree存储引擎的基本数据单位。
-# 该类提供了基本的构造、比较和表示方法，并通过序列化和反序列化方法支持JSON格式的存储。
 class KeyValue:
-    def __init__(self, key, value):  # 构造函数，初始化键值对
-        self.key = key   # 键，可以是任何可哈希的类型
-        self.value = value # 值，可以是任何类型
+    def __init__(self, key: Any, value: Any):
+        # 构造函数，用于创建键值对对象
+        # key: 键，可以是任何可哈希的类型
+        # value: 值，可以是任何类型
+        self.key = key
+        self.value = value
 
-    def __repr__(self):  # 返回对象的字符串表示，有助于调试和日志记录
+    def __repr__(self) -> str:
+        # 返回对象的字符串表示
+        # 用于调试和日志记录
         return f"Key: {self.key}, Value: {self.value}"
 
-    def __eq__(self, other):  # 定义等式检查，用于比较键值对是否相等
-        if isinstance(other, KeyValue): # 确保另一个对象是键值对类型
-            return self.key == other.key and self.value == other.value # 检查键和值是否相等
-        return False # 如果另一个对象不是键值对类型，则返回False
+    def __eq__(self, other: Any) -> bool:
+        # 定义等式检查，用于比较两个键值对是否相等
+        # other: 另一个要比较的对象
+        # 返回: 如果键和值都相等，则为True，否则为False
+        if isinstance(other, KeyValue):
+            return self.key == other.key and self.value == other.value
+        return False
 
-    def __lt__(self, other):  # 定义小于比较，用于排序键值对
-        return str(self.key) < str(other.key)  # 将键转换为字符串后进行比较
+    def __lt__(self, other: Any) -> bool:
+        # 定义小于比较，用于排序键值对
+        # other: 另一个要比较的对象
+        # 返回: 如果当前对象的键小于另一个对象的键，则为True，否则为False
+        return str(self.key) < str(other.key)
 
-    def serialize(self) -> str:  # 序列化方法，将键值对转换为JSON字符串
-        # 针对非基本数据类型转换为字符串，基本类型保持不变
-        key = str(self.key) if not isinstance(self.key, (int, float, str, list, dict)) else self.key
-        value = str(self.value) if not isinstance(self.value, (int, float, str, list, dict)) else self.value
-        return json.dumps({"key": key, "value": value})  # 使用json.dumps将键值对转换为JSON
+    def serialize(self) -> str:
+        # 序列化方法，将键值对转换为JSON字符串
+        # 返回: 表示键值对的JSON字符串
+        key = self._serialize_value(self.key)
+        value = self._serialize_value(self.value)
+        return json.dumps({"key": key, "value": value})
 
-    @staticmethod  # 静态方法，不依赖于类的实例
-    def deserialize(serialized: str):  # 反序列化方法，从JSON字符串创建KeyValue对象
-        data = json.loads(serialized)  # 使用json.loads解析JSON字符串
-        return KeyValue(data["key"], data["value"])  # 从解析的数据中提取键和值，并创建新的KeyValue对象
+    def _serialize_value(self, value: Any) -> Any:
+        # 辅助方法，用于序列化单个值
+        # value: 要序列化的值
+        # 返回: 序列化后的值
+        # 如果值是基本类型，则直接返回
+        # 如果值是元组，则转换为列表
+        # 如果值是集合，则转换为列表
+        # 对于其他类型，转换为字符串
+        if isinstance(value, (int, float, str, list, dict)):
+            return value
+        elif isinstance(value, tuple):
+            return list(value)
+        elif isinstance(value, set):
+            return list(value)
+        else:
+            return str(value)
+
+    @staticmethod
+    def deserialize(serialized: str) -> 'KeyValue':
+        # 静态方法，从JSON字符串创建KeyValue对象
+        # serialized: 表示键值对的JSON字符串
+        # 返回: 新创建的KeyValue对象
+        # 如果字符串不是有效的JSON或缺少键或值，则抛出ValueError异常
+        try:
+            data = json.loads(serialized)
+            return KeyValue(data["key"], data["value"])
+        except json.JSONDecodeError:
+            raise ValueError("Invalid serialized string") from None
+        except KeyError:
+            raise ValueError("Missing key or value in serialized string") from None
 
--- memtable.py
+++ memtable.py
@@ -1,43 +1,50 @@
-from typing import Optional
+from data_model import KeyValue  # 从data_model模块导入KeyValue类
+import json  # 导入JSON库，用于序列化和反序列化字典
+import threading  # 导入线程库，用于同步操作
+from typing import Optional  # 导入Optional类型，用于类型注解
 
-# MemTable是LSM-tree结构的一部分，用于存储最近写入的数据。
-# 当数据达到一定阈值时，MemTable的内容将持久化到SSTable。
-# 这种设计是为了优化写入性能，使其能够快速响应写入请求。
 class MemTable:
-    # 构造函数初始化MemTable，并设置持久化阈值。
-    # threshold参数定义了触发持久化操作的键值对数量。
-    def __init__(self, threshold: int):
-        self.data = {}  # 使用字典存储键值对，提供O(1)的读写性能。
-        self.threshold = threshold  # 设置持久化阈值。
+    def __init__(self, threshold: int = 1000):
+        self.data = {}  # 创建字典以存储键值对
+        self.threshold = threshold  # 设置持久化阈值
+        self.lock = threading.Lock()  # 创建锁，确保线程安全
 
-    # put方法用于插入或更新键值对。
-    # 如果键不存在，将创建新的键值对；如果键已存在，将更新对应的值。
-    def put(self, key: str, value: str):
-        self.data[key] = value  # 插入或更新键值对。
-        # 如果达到持久化阈值，触发compact方法进行持久化操作。
-        if len(self.data) >= self.threshold:
-            self.compact()
+    def put(self, key: str, value: KeyValue) -> None:
+        with self.lock:  # 获取锁，确保线程安全
+            self.data[key] = value  # 将键值对存储在字典中
+            if len(self.data) >= self.threshold:  # 检查是否达到持久化阈值
+                self.compact()  # 如果达到阈值，则持久化数据
 
-    # get方法用于检索给定键的值。
-    # 如果键不存在，将返回None。
-    def get(self, key: str) -> Optional[str]:
-        return self.data.get(key)  # 使用字典的get方法获取值。
+    def get(self, key: str) -> Optional[KeyValue]:
+        with self.lock:  # 获取锁，确保线程安全
+            return self.data.get(key)  # 使用字典的get方法获取值
 
-    # delete方法用于删除给定键的键值对。
-    # 如果键不存在，操作无效。
     def delete(self, key: str):
-        if key in self.data:  # 检查键是否存在。
-            del self.data[key]  # 删除键值对。
+        with self.lock:  # 获取锁，确保线程安全
+            if key in self.data:  # 检查键是否存在
+                del self.data[key]  # 如果存在，则删除
 
-    # compact方法负责将MemTable的内容持久化到SSTable。
-    # 在LSM-tree中，这是将数据从内存转移到磁盘的关键步骤。
-    # 这个方法在实际实现中需要根据SSTable的设计来完成。
     def compact(self):
-        # TODO: 实现持久化逻辑。
-        pass
+        with self.lock:  # 获取锁，确保线程安全
+            with open('memtable.json', 'w') as file:  # 打开文件以写入
+                # 使用键值对的serialize方法将字典写入文件
+                json.dump({k: v.serialize() for k, v in self.data.items()}, file)
+            self.data.clear()  # 清空字典
 
-# 示例代码演示了如何使用MemTable进行基本操作。
-memtable = MemTable(threshold=3)
-memtable.put('name', 'Alice')
-memtable.put('age', '30')
-print(memtable.get('name'))  # 输出 'Alice'
+    def recover(self, log_path: str):
+        try:  # 尝试打开文件
+            with open(log_path, 'r') as file:  # 打开文件以读取
+                data = json.load(file)  # 使用json.load从文件加载字典
+                # 使用键值对的deserialize方法从解析的数据中恢复对象
+                self.data = {k: KeyValue.deserialize(v) for k, v in data.items()}
+        except FileNotFoundError:  # 捕获文件未找到错误
+            print(f"Log file {log_path} not found. Starting with an empty MemTable.")  # 打印错误消息
+
+# 示例代码
+memtable = MemTable(threshold=3)  # 创建MemTable对象，并设置阈值为3
+memtable.put('name', KeyValue('name', 'Alice'))  # 插入键值对
+memtable.put('age', KeyValue('age', '30'))  # 插入键值对
+print(memtable.get('name'))  # 获取并打印值
+memtable.put('city', KeyValue('city', 'New York'))  # 插入键值对，触发compact
+memtable.recover('memtable.json')  # 从文件恢复数据
+print(memtable.get('name'))  # 获取并打印恢复后的值
