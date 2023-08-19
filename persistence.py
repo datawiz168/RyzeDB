4c05aeede1a6ebdfdb0d94cebe138bfecf23eac6
datawiz168 <140295050+datawiz168@users.noreply.github.com>
2023-08-19 20:38:20 +0800

    Add files via upload

--- persistence.py
+++ persistence.py
@@ -1,19 +1,21 @@
-import pickle
-from table import MemTable
+import pickle  # 导入pickle库，用于对象的序列化和反序列化
+from table import MemTable  # 导入MemTable类
 
 class Persistence:
     def __init__(self, log_file_path: str):
-        self.log_file_path = log_file_path
+        self.log_file_path = log_file_path  # 设置日志文件的路径
 
     def persist_memtable(self, memtable: MemTable):
-        # 将MemTable序列化并写入WAL（写前日志）
-        with open(self.log_file_path, 'ab') as file:
-            serialized_data = pickle.dumps(memtable)
-            file.write(serialized_data)
+        # 序列化MemTable的key-value对并写入WAL（写前日志）
+        with open(self.log_file_path, 'wb') as file:  # 以二进制写入模式打开文件
+            serialized_data = pickle.dumps(memtable.data)  # 将MemTable的数据序列化
+            file.write(serialized_data)  # 将序列化后的数据写入文件
 
     def recover_memtable(self) -> MemTable:
-        # 从WAL（写前日志）恢复MemTable
-        with open(self.log_file_path, 'rb') as file:
-            serialized_data = file.read()
-            memtable = pickle.loads(serialized_data)
-            return memtable
+        # 从WAL恢复key-value对并重构MemTable
+        with open(self.log_file_path, 'rb') as file:  # 以二进制读取模式打开文件
+            serialized_data = file.read()  # 读取序列化的数据
+            data = pickle.loads(serialized_data)  # 反序列化数据
+            memtable = MemTable()  # 创建MemTable对象
+            memtable.data = data  # 将反序列化的数据赋给MemTable
+            return memtable  # 返回重构后的MemTable
--- test_persistence.py
+++ test_persistence.py
@@ -1,30 +1,18 @@
-import unittest
-from table import MemTable
-from persistence import Persistence
+import unittest  # 导入unittest库，用于单元测试
+from persistence import Persistence  # 导入Persistence类
+from table import MemTable  # 导入MemTable类
 
-class TestPersistence(unittest.TestCase):
-    def setUp(self):
-        self.log_file_path = 'wal.log'
-        self.memtable = MemTable()
-        self.memtable.set('key1', 'value1')
-        self.memtable.set('key2', 'value2')
-        self.persistence = Persistence(self.log_file_path)
+class TestPersistence(unittest.TestCase):  # 定义测试类
+    def setUp(self):  # 测试前的设置
+        self.memtable = MemTable()  # 创建MemTable对象
+        self.memtable.put('key1', 'value1')  # 向MemTable中添加数据
+        self.persistence = Persistence('test_log_file.log')  # 创建Persistence对象
 
-    def test_persist_and_recover_memtable(self):
-        # 测试将MemTable持久化到WAL
-        self.persistence.persist_memtable(self.memtable)
-
-        # 从WAL中恢复MemTable
-        recovered_memtable = self.persistence.recover_memtable()
-
-        # 验证恢复后的MemTable与原始MemTable相同
-        self.assertEqual(self.memtable.get('key1'), recovered_memtable.get('key1'))
-        self.assertEqual(self.memtable.get('key2'), recovered_memtable.get('key2'))
-
-    def tearDown(self):
-        # 清理测试生成的文件
+    def tearDown(self):  # 测试后的清理
         import os
-        os.remove(self.log_file_path)
+        os.remove('test_log_file.log')  # 删除测试日志文件
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_persist_and_recover_memtable(self):  # 测试持久化和恢复MemTable
+        self.persistence.persist_memtable(self.memtable)  # 持久化MemTable
+        recovered_memtable = self.persistence.recover_memtable()  # 恢复MemTable
+        self.assertEqual(recovered_memtable.get('key1'), 'value1')  # 验证恢复的数据
